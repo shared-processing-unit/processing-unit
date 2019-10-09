f2c23357b4cbfc8a8f5e70cca9d47d3bfa90a686
rkram3r <skill8r@gmail.com>
2019-10-09 16:30:53 +0200

    remove.

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,12 @@
 import DOMWebSocketMapper from './DOMWebSocketMapper'
 
-const callFunctionIfDOMReady = (func: () => void) => {
-    const domReady =
-        document.readyState === 'complete' ||
-        document.readyState === 'interactive'
-    domReady ? func() : setTimeout(() => callFunctionIfDOMReady(func), 1)
-}
+const webSocket = new WebSocket(process.env.connection)
+const scriptTag = document.createElement('script')
+document.body.appendChild(scriptTag)
 
-callFunctionIfDOMReady(() => {
-    const webSocket = new WebSocket(process.env.connection)
-    const scriptTag = document.createElement('script')
-    document.body.appendChild(scriptTag)
-
-    const mapper = new DOMWebSocketMapper(
-        webSocket,
-        scriptTag,
-        window.location.href
-    )
-    mapper.mapWebSocketToDOMElement()
-})
+const mapper = new DOMWebSocketMapper(
+    webSocket,
+    scriptTag,
+    window.location.href
+)
+mapper.mapWebSocketToDOMElement()
