592771a7e5cd22efbcf5899d9c51ca478ff39c1c
rkram3r <skill8r@gmail.com>
2019-10-09 15:35:02 +0200

    call function if dom ready.

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,21 @@
 import DOMWebSocketMapper from './DOMWebSocketMapper'
 
-const webSocket = new WebSocket(process.env.connection)
-const scriptTag = document.createElement('script')
-document.body.appendChild(scriptTag)
+const callFunctionIfDOMReady = (func: () => void) => {
+    const domReady =
+        document.readyState === 'complete' ||
+        document.readyState === 'interactive'
+    domReady ? func() : setTimeout(() => callFunctionIfDOMReady(func), 1)
+}
 
-const mapper = new DOMWebSocketMapper(
-    webSocket,
-    scriptTag,
-    window.location.href
-)
-mapper.mapWebSocketToDOMElement()
+callFunctionIfDOMReady(() => {
+    const webSocket = new WebSocket(process.env.connection)
+    const scriptTag = document.createElement('script')
+    document.body.appendChild(scriptTag)
+
+    const mapper = new DOMWebSocketMapper(
+        webSocket,
+        scriptTag,
+        window.location.href
+    )
+    mapper.mapWebSocketToDOMElement()
+})
