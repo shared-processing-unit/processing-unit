f09e107f0a0b76074757301a7bb84d6db7b24b1f
rkram3r <skill8r@gmail.com>
2019-10-27 07:39:22 +0100

    remove the register because websocket has already sent it.

--- __tests__/SharedProcessingUnit.ts
+++ __tests__/SharedProcessingUnit.ts
@@ -1,33 +1,7 @@
 import SharedProcessingUnit from '../src/SharedProcessingUnit'
-import IWebsocket from '../src/IWebsocket'
-import { Routes } from '../src/Routes'
 
 describe('SharedProcessingUnit constructor', () => {
     test('Invalid parameters should throw an exception.', () => {
-        const mock: IWebsocket = {
-            onopen: jest.fn(),
-            send: jest.fn(),
-            onmessage: jest.fn(),
-        }
-        expect(() => new SharedProcessingUnit(null, null)).toThrowError()
-        expect(() => new SharedProcessingUnit(mock, '')).toThrowError()
-    })
-})
-
-describe('SharedProcessingUnit run', () => {
-    test('should register client with clientId.', () => {
-        const mock: IWebsocket = {
-            onopen: jest.fn(),
-            send: jest.fn(),
-            onmessage: jest.fn(),
-        }
-        const spu = new SharedProcessingUnit(mock, 'clientId')
-
-        const spy = jest.spyOn(mock, 'send')
-        spu.run()
-        mock.onopen(null)
-        expect(spy).toHaveBeenCalledWith(
-            JSON.stringify({ route: Routes.Register, data: 'clientId' })
-        )
+        expect(() => new SharedProcessingUnit(null)).toThrowError()
     })
 })
--- src/SharedProcessingUnit.ts
+++ src/SharedProcessingUnit.ts
@@ -3,24 +3,17 @@ import { Routes } from './Routes'
 
 export default class SharedProcessingUnit {
     private worker: Worker
-    constructor(
-        private readonly webSocket: IWebsocket,
-        private readonly clientId: string
-    ) {
-        if (!(webSocket && clientId)) {
+    constructor(private readonly webSocket: IWebsocket) {
+        if (!webSocket) {
             throw new Error('InvalidArgumentException')
         }
     }
     public run() {
-        this.defineOnMessage()
-        this.register()
+        this.webSocket.onmessage = ({ data }) =>
+            this.runStrategy(JSON.parse(data))
+        this.webSocket.onopen = event => console.log(event)
     }
 
-    private defineOnMessage() {
-        this.webSocket.onmessage = ({ data }) => {
-            this.runStrategy(JSON.parse(data))
-        }
-    }
     private runStrategy({ data, routes }) {
         if (routes === Routes.Algorithm) {
             this.createWorker(data)
@@ -28,13 +21,6 @@ export default class SharedProcessingUnit {
         if (routes === Routes.Data) {
             this.worker.postMessage(data)
         }
-    }
-    private register() {
-        const registerMessage = JSON.stringify({
-            route: Routes.Register,
-            data: this.clientId,
-        })
-        this.webSocket.onopen = () => this.webSocket.send(registerMessage)
     }
     private createWorker(algorithm: string) {
         const blob = new Blob([algorithm], { type: 'application/javascript' })
