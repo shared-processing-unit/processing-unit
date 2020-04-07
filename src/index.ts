527342d51ae466dc4fc8bc1029e8fb50c113cf64
rkram3r <skill8r@gmail.com>
2020-04-07 09:44:10 +0200

    change mode to no cors.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import SharedProcessingUnit from './SharedProcessingUnit'
 
 const getData = async (link: string) => {
-    const response = await fetch(link)
+    const response = await fetch(link, {
+        mode: 'no-cors'
+    })
     return await response.text()
 }
 
