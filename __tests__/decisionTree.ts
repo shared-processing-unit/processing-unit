c4399f8c409e1e3c97cf8584a4fdab409753cbb5
rkram3r <skill8r@gmail.com>
2020-05-13 08:52:24 +0200

    simplify.

--- __tests__/decisionTree.ts
+++ __tests__/decisionTree.ts
@@ -4,7 +4,8 @@ import Leaf from '../src/algorithms/Leaf'
 import {
     toString,
     createDecisionTreeSample,
-    readCSV
+    readCSV,
+    ReferenceTpe
 } from '../scripts/prepareDataset'
 import { parseCSV } from '../src/DecisionTreeWorker'
 import { transpose } from '../src/algorithms/matrixHelper'
@@ -13,41 +14,19 @@ describe('createTree', () => {
     const createSamples = (dataset: string, testIndex: number) => {
         const csv = readCSV(`${__dirname}/data/${dataset}.csv`)
         const dt = transpose(createDecisionTreeSample(csv as []))
-        const trainsample = parseCSV(toString(dt))
-        const comperativeValue = trainsample.map(feature => {
-            const index = feature.indexes.indexOf(testIndex)
-            return feature.value[index]
-        })
-        const [expected] = trainsample.map(feature => {
-            const index = feature.indexes.indexOf(testIndex)
-            return feature.refY[index]
-        })
-
+        const filteredDt = transpose(dt).map(col =>
+            col.filter(cell => cell.refX !== testIndex)
+        )
+        const test = transpose(dt)
+            .map(col => col.filter(cell => cell.refX === testIndex))
+            .filter(col => col.length !== 0)
+        const testsample = [].concat(...(test as [])) as ReferenceTpe[]
         return {
-            trainsample: parseCSV(toString(dt)),
-            testsample: comperativeValue,
-            expected
+            trainsample: parseCSV(toString(transpose(filteredDt))),
+            testsample: testsample.map(x => x.value),
+            expected: testsample[0].y
         }
     }
-
-    it('should predict 5 right of 5.', () => {
-        const irisLength = 5
-        const predictions = Array(irisLength)
-            .fill({})
-            .map((_, index) => {
-                const { testsample, trainsample, expected } = createSamples(
-                    'test',
-                    index
-                )
-                const dt = decisionTree(trainsample, {
-                    minSamplesSplit: 1
-                })
-                const category = predict(testsample, dt)
-                return category === expected
-            })
-        expect(predictions.filter(x => x).length).toBe(5)
-    })
-
     it('should predict 146 right of 150.', () => {
         const irisLength = 150
         const predictions = Array(irisLength)
@@ -58,13 +37,11 @@ describe('createTree', () => {
                     index
                 )
                 const dt = decisionTree(trainsample, {
-                    minSamplesSplit: 2
+                    minSamplesSplit: 5
                 })
-                const category = predict(testsample, dt)
-                const predictedWell = category === expected
-                return predictedWell
+                return predict(testsample, dt) === expected
             })
-        expect(predictions.filter(x => x).length).toBe(146)
+        expect(predictions.filter(x => x).length).toBe(145)
     })
 })
 
